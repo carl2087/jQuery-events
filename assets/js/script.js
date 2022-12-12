ed4d97eda527138a753c48daa6335bba80fc2fb4
Carl <carlelli5@hotmail.co.uk>
2022-12-12 20:30:53 +0000

    added examples of jquery code

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,49 @@ $("#stream3_btn").on("click", function() {
         $(".stream3").addClass('highlight_stream');
 });
 }); 
+
+// challenge and jquery events examples
+
+// $("p").click(function() {
+//         $("p").css("color", "red");
+//     });
+
+//     $("h2").hover(function() {
+//         $("h2").css("background", "lightblue");
+//     });
+
+//     $(".card-panel").mouseenter(function() {
+//         $("body").css("background-color", "black");
+//     });
+
+//     $(".card-panel").mouseleave(function() {
+//         $("body").css("background-color", "#e1e2e2");
+//     });
+
+// jQuery effects examples
+
+// $("#card-panel-1").click(function() {
+//         $("#card-panel-1").hide("slow");
+//     });
+
+//     $("#card-panel-2").click(function() {
+//         $("#card-panel-2").hide("medium");
+//     });
+
+//     $("#card-panel-3").click(function() {
+//         $("#card-panel-3").hide("fast");
+//     });
+
+//     $("#card-panel-4").click(function() {
+//         $("#card-panel-4").hide(3000);
+//     });
+
+// method chaining examples
+
+// $("button").mouseenter(function(){
+//         $("button").removeClass("make-red").addClass("make-border");
+//     });
+
+//     $("button").mouseleave(function() {
+//         $("button").removeClass("make-border").addClass("make-red");
+//     });
